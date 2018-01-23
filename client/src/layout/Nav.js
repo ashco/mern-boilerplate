b171413852d5225ef037664009692ec92678fa5a
ashco <ashchristie@gmail.com>
2018-01-23 10:18:43 -0800

    init setup

---
+++ client/src/layout/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+	render(){
+		return(
+			<div>
+				<nav className="nav">
+					<a href="/">Home</a>
+					<a href="/">Test 1</a>
+					<a href="/">Test 2</a>
+				</nav>
+			</div>
+		);
+	}
+}
+
+export default Nav;
